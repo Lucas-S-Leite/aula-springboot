e3683d8b575ab64467ce4e579ae2f52b15031292
dti <dti@LAB204-14>
2023-09-12 11:19:52 -0300

    Aula 12-09-23

--- produto/src/main/java/com/fatec/produto/servico/IProdutoServico.java
+++ produto/src/main/java/com/fatec/produto/servico/IProdutoServico.java
@@ -1,9 +1,22 @@
 package com.fatec.produto.servico;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
-import com.fatec.produto.model.Produto;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fatec.produto.model.Catalogo;
+import com.fatec.produto.model.Imagem;
 
 public interface IProdutoServico {
-	public List<Produto> consultaCatalogo();
+	public Optional<Imagem> salvar(MultipartFile arquivo, long id) throws IOException;
+
+	public List<Imagem> getAll();
+
+	public byte[] getImagem(String nomeArquivo);
+
+	public byte[] getImagemById(Long id);
+
+	public List<Catalogo> consultaCatalogo();
 }
--- produto/src/main/java/com/fatec/produto/servico/ProdutoServico.java
+++ produto/src/main/java/com/fatec/produto/servico/ProdutoServico.java
@@ -1,21 +1,66 @@
 package com.fatec.produto.servico;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fatec.produto.model.Catalogo;
+import com.fatec.produto.model.IImagemRepository;
 import com.fatec.produto.model.IProdutoRepository;
+import com.fatec.produto.model.Imagem;
 import com.fatec.produto.model.Produto;
 
 @Service
 public class ProdutoServico implements IProdutoServico {
 	@Autowired
-	IProdutoRepository repository;
+	IProdutoRepository repositoryP;
+	@Autowired
+	IImagemRepository repositoryI;
 
 	@Override
-	public List<Produto> consultaCatalogo() {
-		return repository.findAll();
+	public List<Catalogo> consultaCatalogo() {
+		Catalogo c = null;
+		List<Catalogo> lista = new ArrayList<>();
+		List<Produto> listaP = repositoryP.findAll();
+		List<Imagem> listaI = repositoryI.findAll();
+		for (Produto p : listaP) {
+			for (Imagem i : listaI) {
+				if (p.getId().equals(i.getId())) {
+					c = new Catalogo(p.getId(), p.getDescricao(), p.getCategoria(), p.getQuantidadeNoEstoque(), i.getArquivo());
+					lista.add(c);
+				}
+			}
+		}
+		return lista;
+	}
+
+	@Override
+	public Optional<Imagem> salvar(MultipartFile arquivo, long id) throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Imagem> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] getImagem(String nomeArquivo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public byte[] getImagemById(Long id) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
