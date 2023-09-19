706dac6c9ecc9c89b5e1fcc7d3aa807375f2860d
dti <dti@LAB204-14>
2023-09-19 09:47:14 -0300

    Aula 19-09-2023

--- produto/src/main/java/com/fatec/produto/servico/LoadDatabase.java
+++ produto/src/main/java/com/fatec/produto/servico/LoadDatabase.java
@@ -2,24 +2,78 @@ package com.fatec.produto.servico;
 
 import java.util.Arrays;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import com.fatec.produto.model.IImagemRepository;
 import com.fatec.produto.model.IProdutoRepository;
+import com.fatec.produto.model.Imagem;
 import com.fatec.produto.model.Produto;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Configuration
 public class LoadDatabase {
-	@Bean
-	CommandLineRunner initDatabase (IProdutoRepository repository) {
-		return args -> {
-			Produto produto1 = new Produto("Eletrobomba 110V para Maquina de Lavar e Lava Louças", "maquina de lavar", 51.66, 12);
-			Produto produto2 = new Produto("Tirante Original Brastemp E Consul De 7 A 12 Kg 11cm","lavar louça", 3.90,20);
-			Produto produto3 = new Produto("Termoatuador Lavadora Colormaq Electrolux GE","maquina de lavar", 29.70,40);
+    Logger logger = LogManager.getLogger(this.getClass());
+    
+    @Autowired
+    IImagemRepository IImagemRepository;
+    
+    @Bean
+    CommandLineRunner initDatabase(IProdutoRepository repository) {
+        return args -> {
+            Produto produto1 = new Produto("Eletrobomba 110V para Maquina de Lavar e Lava Louças", "maquina de lavar",
+                    51.66, 12);
+            Produto produto2 = new Produto("Tirante Original Brastemp E Consul De 7 A 12 Kg 11cm", "lavar louça", 3.90,
+                    20);
+            Produto produto3 = new Produto("Termoatuador Lavadora Colormaq Electrolux GE", "maquina de lavar", 29.70,
+                    40);
+            repository.saveAll(Arrays.asList(produto1, produto2, produto3));
+            logger.info(">>>>> loaddatabase -> cadastro de 3 produtos realizado.");
+            // ****************************************************************
+            // upload - obtem a imagem do c, atribui ao obj imagem e salva no db do servidor
+            // ****************************************************************
+            Path path = Paths.get("C:/Users/dti/Downloads/eletrobomba.png");
+            InputStream file = Files.newInputStream(path);
+            byte[] arquivo1 = file.readAllBytes();
+            Imagem imagem = new Imagem();
+            imagem.setId(1L); // associa o id do produto ao id da imagem
+            imagem.setNome("eletrobomba.png");
+            imagem.setCaminho("imagens/" + imagem.getNome());
+            imagem.setArquivo(arquivo1);
+            logger.info(">>>>> loaddatabase -> upload de arquivo imagem realizado => " + arquivo1.length);
+            IImagemRepository.save(imagem);
+            // ****************************************************************
+            path = Paths.get("C:/Users/dti/Downloads/helice.png");
+            file = Files.newInputStream(path);
+            byte[] arquivo2 = file.readAllBytes();
+            imagem = new Imagem();
+            imagem.setId(2L); // associa o id do produto ao id da imagem
+            imagem.setNome("helice.png");
+            imagem.setCaminho("imagens/" + imagem.getNome());
+            imagem.setArquivo(arquivo2);
+            logger.info(">>>>> loaddatabase -> upload de arquivo imagem realizado => " + arquivo2.length);
+            IImagemRepository.save(imagem);
+            // ****************************************************************
+            path = Paths.get("C:/Users/dti/Downloads/pressurizadora.png");
+            file = Files.newInputStream(path);
+            byte[] arquivo3 = file.readAllBytes();
+            imagem = new Imagem();
+            imagem.setId(3L); // associa o id do produto ao id da imagem
+            imagem.setNome("pressurizadora.png");
+            imagem.setCaminho("imagens/" + imagem.getNome());
+            imagem.setArquivo(arquivo3);
+            logger.info(">>>>> loaddatabase -> upload de arquivo imagem realizado => " + arquivo3.length);
+            IImagemRepository.save(imagem);
+        };
 
-			repository.saveAll(Arrays.asList(produto1, produto2, produto3));
-		};
-	}
+    }
 
 }
